5372faf8b8192201495d8c7bd2f6a3c5e94174e2
NataliaMGonzalez <nataliagzzh@gmail.com>
2020-09-06 19:11:13 -0500

    HW3 finished

--- HW3Intents/app/src/main/java/com/example/hw3intents/MainActivity.java
+++ HW3Intents/app/src/main/java/com/example/hw3intents/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.hw3intents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.hw3intents.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    public static int counter = 0;
+    private Button mBtnSayHello;
+    private Button mBtnCount;
+    private TextView mTextViewCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnSayHello = findViewById(R.id.BtnSayHello);
+        mBtnCount = findViewById(R.id.BtnCount);
+        mTextViewCounter = findViewById(R.id.TextViewCount);
+        mTextViewCounter.setText(String.valueOf(counter));
+
+        mBtnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                mTextViewCounter.setText(String.valueOf(counter));
+            }
+        });
     }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = String.valueOf(counter);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                counter = parseInt(reply);
+                mTextViewCounter.setText(reply);
+
+            }
+        }
+    }
+
 }
--- HW3Intents/app/src/main/java/com/example/hw3intents/SecondActivity.java
+++ HW3Intents/app/src/main/java/com/example/hw3intents/SecondActivity.java
@@ -2,13 +2,61 @@ package com.example.hw3intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.hw3intents.extra.REPLY";
+
+    private TextView mTextViewCounter;
+    private EditText mEditTextNewValue;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mTextViewCounter = findViewById(R.id.TextViewCountHello);
+        mEditTextNewValue = findViewById(R.id.EditTextNewValue);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        mTextViewCounter.setText(String.valueOf(message));
+
     }
+
+    // Back
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case android.R.id.home:
+
+                String newCounter = mEditTextNewValue.getText().toString();
+                Intent replyIntent = new Intent();
+                replyIntent.putExtra(EXTRA_REPLY, newCounter);
+                setResult(RESULT_OK, replyIntent);
+                finish();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        String newCounter = mEditTextNewValue.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY,newCounter);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+
 }
