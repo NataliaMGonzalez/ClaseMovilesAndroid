107d9714041869472bd96d28c3c53610f8957255
NataliaMGonzalez <nataliagzzh@gmail.com>
2020-10-08 23:05:55 -0500

    Fragments part one finished, week 7

--- FragmentExample/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
+++ FragmentExample/app/src/main/java/com/example/android/fragmentexample/SimpleFragment.java
@@ -5,14 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link SimpleFragment#newInstance} factory method to
  * create an instance of this fragment.
- *
  */
 public class SimpleFragment extends Fragment {
+    // yes at index 0, no at index 1
+    private static final int yes = 0;
+    private static final int no  = 1;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -23,41 +27,62 @@ public class SimpleFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SimpleFragment newInstance(String param1, String param2) {
-        SimpleFragment fragment = new SimpleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public SimpleFragment() {
         // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static SimpleFragment newInstance() {
+        SimpleFragment fragment = new SimpleFragment();
+        Bundle args = new Bundle();
+        // args.putString(ARG_PARAM1, param1);
+        // args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+         //   mParam1 = getArguments().getString(ARG_PARAM1);
+         //   mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        final RadioGroup radioGroup = rootView.findViewById(R.id.radio_group);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // clicked button
+                View radioButton = radioGroup.findViewById(i);
+                int index = radioGroup.indexOfChild(radioButton);
+
+                TextView txtView = rootView.findViewById(R.id.fragmentHeader);
+
+                switch (index){
+                    case yes: //user chose yes
+                        txtView.setText(R.string.yes_message);
+                        break;
+
+                    case no:
+                        txtView.setText(R.string.no_message);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        return rootView;
     }
 }
